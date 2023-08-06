3324b5a67f52fc57c0efee6d097eadf4e9b42a24
Kevin-Dowling <90491109+Kevin-Dowling@users.noreply.github.com>
2023-08-07 00:41:33 +1200

    Revert "rot rev"
    This reverts commit 756303e981b912851502ae44d341d124f2fbf5e3.

--- main.js
+++ main.js
@@ -44,6 +44,8 @@ video.addEventListener('play', () => {
 // Three.js render loop
 function animate() {
   requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
